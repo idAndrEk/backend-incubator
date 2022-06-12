17d0781e403f1642f42072fb1da3243f38e0e755
Andrey <AndrEkat96@yandex.ru>
2022-06-12 17:02:18 +0500

    home

--- src/index.ts
+++ src/index.ts
@@ -9,52 +9,41 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 5000
 
-const videos = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+const bloggers = [
+    {id: 1, name: 'IT-KAMASUTRA', youtubeUrl: 'https://www.youtube.com/c/ITKAMASUTRA'},
+    {id: 2, name: 'webDev', youtubeUrl: 'https://www.youtube.com/c/YauhenKavalchuk'},
+    {id: 3, name: 'Egor Malkevich', youtubeUrl: 'https://www.youtube.com/c/EgorMalkevich'},
+    {id: 4, name: 'Ulbi TV', youtubeUrl: 'https://www.youtube.com/c/UlbiTV'},
 ]
 
-app.get('/videos', (req: Request, res: Response) => {
-    res.send(videos)
+app.get('/bloggers', (req: Request, res: Response) => {
+    res.send(bloggers)
 });
 
-app.get('/videos/:videoId', (req: Request, res: Response) => {
-    const id = +req.params.videoId;
-    const video = videos.find(v => v.id === id)
-    if(!video) {
-        res.sendStatus(404)
+app.get('/bloggers/:bloggerId', (req: Request, res: Response) => {
+    const id = +req.params.bloggerId;
+    const blogger = bloggers.find(b => b.id === id)
+    if(!blogger) {
+        res.sendStatus(404).send('Not found')
     } else {
-        res.json(video)
+        res.json(blogger)
     }
 })
 
-app.post('/videos', (req: Request, res: Response) => {
-    let title = req.body.title
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
-        res.status(400).send({
-            errorsMessages: [{
-                    message: "maxLength 40",
-                    field: "title"
-                }]
-        })
-        return;
+app.post('/bloggers', (req: Request, res: Response) => {
+        const newBlogger = {
+        id: +req.body.id,
+        name: req.body.name,
+            youtubeUrl: req.body.youtubeUrl
     }
-    const newVideo = {
-        id: +(new Date()),
-        title: req.body.title,
-        author: 'it-incubator.eu'
-    }
-    videos.push(newVideo)
-    res.status(201).send(newVideo)
+    bloggers.push(newBlogger)
+    res.status(201).send(newBlogger)
 })
 
-app.delete('/videos/:id',(req: Request, res: Response)=>{
-    for (let i = 0; i < videos.length; i++) {
-        if (videos[i].id === +req.params.id) {
-            videos.splice(i, 1);
+app.delete('/bloggers/:id',(req: Request, res: Response)=>{
+    for (let i = 0; i < bloggers.length; i++) {
+        if (bloggers[i].id === +req.params.id) {
+            bloggers.splice(i, 1);
             res.send(204)
             return
         }
@@ -62,34 +51,15 @@ app.delete('/videos/:id',(req: Request, res: Response)=>{
     res.send(404)
 })
 
-// app.delete('/videos/:id',(req: Request, res: Response)=>{
-//     const index = videos.findIndex(v => v.id === +req.params.id)
-//     if (index === -1) {
-//         res.send(404)
-//     } else {
-//         videos.splice(index, 1)
-//         res.send(204)
-//     }
-// })
-
-app.put('/videos/:videoId',(req: Request, res: Response)=>{
-    let title = req.body.title
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
-        res.status(400).send({
-            errorsMessages: [{
-                    message: "maxLength 40",
-                    field: "title"
-                }]
-        })
-        return;
-    }
-    const id = +req.params.videoId;
-    const video = videos.find(v => v.id === id)
-    if (video) {
-        video.title = req.body.title
-        res.status(204).send(video)
+app.put('/bloggers/:bloggerId',(req: Request, res: Response)=>{
+    const id = +req.params.bloggerId;
+    const blogger = bloggers.find(n => n.id === id);
+      if(blogger) {
+          blogger.name = req.body.name,
+              blogger.youtubeUrl = req.body.youtubeUrl
+        res.send(blogger)
     } else {
-        res.send(404);
+        res.send(404)
     }
 })
 
