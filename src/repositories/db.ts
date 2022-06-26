f717deea9093346485498e1fc1f0e1cbfe6863df
Andrey <AndrEkat96@yandex.ru>
2022-06-26 22:05:01 +0500

    Add env

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -29,7 +29,7 @@ export async function runDb() {
         await client.db("home-bloggers").command({ping: 1});
         console.log("Connected successfully to mongo server")
     } catch {
-        console.log("Cant`t connect to db")
+        console.log("Cant`t connect to db!")
         await client.close();
     }
 }
