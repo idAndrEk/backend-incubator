eb410a341ea990ffcfb36610935c35ae630fb605
Andrey <AndrEkat96@yandex.ru>
2022-08-23 14:22:50 +0500

    tasks_add_registration

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -23,7 +23,7 @@ authRouter.post('/registration',
         //if(findByLoginOrEmail) && if(findUserByLogin)
         const user = await usersService.createUser(req.body.login, req.body.email, req.body.password)
         if (user === '250') {
-            res.sendStatus(201)
+            res.sendStatus(204)
             return
         } else {
             res.sendStatus(400)
@@ -35,7 +35,7 @@ authRouter.post('/confirm-email',
     async (req: Request, res: Response) => {
         const result = await authService.confirmEmail(req.body.code)
         if (result) {
-            res.sendStatus(204)
+            res.sendStatus(201)
         } else {
             res.sendStatus(400)
         }
