d567bf9851d8d6a1ea1ca5ed9d7eaf7bc3af1516
Andrey <AndrEkat96@yandex.ru>
2022-10-03 16:03:55 +0500

    comment PUT

--- src/router/comments-router.ts
+++ src/router/comments-router.ts
@@ -5,14 +5,14 @@ import {allValidation} from "../middlewares/ValidationError";
 import {checkIdParamMiddleware} from "../middlewares/checkIdParam-Middleware";
 import {container} from "../composition-root";
 import {CommentsController} from "../controllers/comments-controller";
-import {checkUserTokenMiddleware} from "../middlewares/JwtAuth-Middleware";
+import {JwtAuthMiddleware} from "../middlewares/JwtAuth-Middleware";
 
 const commentsController = container.resolve(CommentsController)
 
 export const commentsRouter = Router({})
 
 commentsRouter.get('/:id', checkIdParamMiddleware, commentsController.getComment.bind(commentsController))
-commentsRouter.put('/:id/like-status', checkUserTokenMiddleware, allValidation, commentsController.addLikeToComment.bind(commentsController))
+commentsRouter.put('/:id/like-status', JwtAuthMiddleware, allValidation, commentsController.addLikeToComment.bind(commentsController))
 commentsRouter.put('/:id', checkIdParamMiddleware, authMiddlewareUser, commentValidation, allValidation, commentsController.updateComment.bind(commentsController))
 commentsRouter.delete('/:id', checkIdParamMiddleware, authMiddlewareUser, commentsController.deleteComment.bind(commentsController))
 
