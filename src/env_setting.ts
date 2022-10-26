f02bfa5f28b666f425662274bee7e63917158d67
user <1234>
2022-10-26 01:42:29 -0700

    ht9 add env vars

--- src/env_setting.ts
+++ src/env_setting.ts
@@ -5,5 +5,7 @@ export const envSetting = {
     MongooseURI: process.env.MongoURI || "mongodb://0.0.0.0:27017",
     PORT: process.env.PORT || 5000,
     JWT_ACCESS: process.env.JWT_ACCESS_SECRET || 'SECRET',
-    JWT_REFRESH: process.env.JWT_REFRESH_SECRET|| 'SECRET'
+    JWT_REFRESH: process.env.JWT_REFRESH_SECRET|| 'SECRET',
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || '10s',
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || '20s',
 }
