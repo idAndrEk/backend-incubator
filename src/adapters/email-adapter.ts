e0c5aa3705996cc9d7e86dae1edfc8b6438c491e
user <1234>
2022-11-13 13:25:41 +0500

    vercel auto deploy test

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -16,10 +16,6 @@ export const emailAdapter = {
             text: `https://somesite.com/confirm-email?code=${code}`
         })
         return result
-        // if (250) {
-        //     console.log(result.response) // if 250 => OK
-        //     return (result.response).split(' ')[0]
-        // }
     },
 
     async sendEmailRecoveryMessage(code: string, email: string) {
