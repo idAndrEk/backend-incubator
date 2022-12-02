34bc5022ec1cbc43ae4755379bc717242e9d882c
Andrey <AndrEkat96@yandex.ru>
2022-12-02 23:55:42 +0500

    loginOrEmail

--- src/middlewares/LoginPassword-Middleware.ts
+++ src/middlewares/LoginPassword-Middleware.ts
@@ -5,7 +5,7 @@ import {UsersService} from "../domain/users-service";
 const usersService = container.resolve(UsersService)
 
 export const LoginPasswordMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await usersService.getUserByLogin(req.body.login)
+    const user = await usersService.getUserByLogin(req.body.loginOrEmail)
     if (!user) return res.status(401).send('Not registered')
 
     const password = await usersService.checkPassword(req.body.password, user.accountData.passwordHash)
